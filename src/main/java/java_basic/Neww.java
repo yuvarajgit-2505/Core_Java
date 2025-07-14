33a3d19087375e90afa0ec736a8ec51e1a965e9a
yuvarajgit-2505 <yuvaraj25525@gmail.com>
2025-07-14 22:30:31 +0530

    neww class added

---
+++ src/main/java/java_basic/Neww.java
@@ -0,0 +1,27 @@
+package java_basic;
+import java.util.Scanner;
+
+public class Neww {
+    static String name="yuva";
+    int rollno=21;
+
+    public void makrs(){
+        int maths=89;
+        int enlish=92;
+        Scanner scam=new Scanner(System.in);
+        System.out.println(name);
+        System.out.println(rollno);
+        System.out.println("Maths marks is"+maths);
+        System.out.println("Enlish mark is"+enlish);
+        System.out.println("Enter the mark in Science");
+        int Science= scam.nextInt();
+        int total=maths+enlish+Science;
+        System.out.println("Total mark is "+total);
+
+    }
+    public static void main(String[] args) {
+        Neww obj= new Neww();
+        obj.makrs();
+    }
+}
+
